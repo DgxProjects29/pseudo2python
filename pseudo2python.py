673879f6ae1bec42e7e8848e893dc57ab52adc20
DgxProjects29 <inevaup@gmail.com>
2021-06-01 09:26:28 -0500

    feat: add support for stacks and queues

--- pseudo2python.py
+++ pseudo2python.py
@@ -13,6 +13,14 @@ with open("mappings/keywords_to_ignore.json", encoding='utf8') as rd:
 with open("mappings/linkedlist_functions.json", encoding='utf8') as rd:
     linkexp_map = json.load(rd)
 
+with open("mappings/stack_queues.json", encoding='utf8') as rd:
+    stack_queue_expressions = json.load(rd)
+
+
+post_regex_expressions = []
+post_simple_replace_expression = []
+header_lines = []
+
 def parse_linklist_line(line):
     matchs = re.finditer(linkexp_map['pattern'], line)
     for match in matchs:
@@ -21,8 +29,66 @@ def parse_linklist_line(line):
         line = line.replace(linkfun, parsed_linkfun)
     return line
 
+
+previous_names = set()
+
+def look_for_stack_queues_expressions(line):
+
+
+    stack_expressions = stack_queue_expressions['stack']
+    queues_expressions = stack_queue_expressions['queue']
+
+    for pattern in stack_expressions:
+        match = re.search(pattern['pattern'], line)
+        if match:
+            name = match.group(1)
+            top_name = match.group(2)
+            base_name = match.group(3)
+            if name not in previous_names:
+                header_lines.append(f"{name} = PseudoStack()")
+                post_regex_expressions.append({
+                    'replace_exp': rf"\b{top_name}\b",
+                    'to_replace': f"{name}.top"
+                })
+                post_regex_expressions.append({
+                    'replace_exp': rf"\b{base_name}\b",
+                    'to_replace': f"{name}.size"
+                })
+            previous_names.add(name)
+            line = re.sub(
+                pattern['pattern'], 
+                pattern['pyeq'],
+                line
+            )
+
+    for pattern in queues_expressions:
+        match = re.search(pattern['pattern'], line)
+        if match:
+            name = match.group(1)
+            base_name = match.group(2)
+            top_name = match.group(3)
+            if name not in previous_names:
+                header_lines.append(f"{name} = PseudoQueue()")
+                post_regex_expressions.append({
+                    'replace_exp': rf"\b{top_name}\b",
+                    'to_replace': f"{name}.top"
+                })
+                post_regex_expressions.append({
+                    'replace_exp': rf"\b{base_name}\b",
+                    'to_replace': f"{name}.base"
+                })
+            previous_names.add(name)
+            line = re.sub(
+                pattern['pattern'], 
+                pattern['pyeq'],
+                line
+            )
+    
+    return line
+
+
 def parse_linklist_funcs_rec(result, funcname, text):
-    match = re.match(linkexp_map['pattern'], text)
+    match = re.search(linkexp_map['pattern'], text)
     if not match:
         exp = f"{text}{linkexp_map[funcname]}"
         return result + exp
@@ -76,6 +142,22 @@ def apply_conditonal_pattern(line):
 
     return line
 
+
+def apply_remaining_expressions(line):
+
+    for exp in post_simple_replace_expression:
+        line = line.replace(exp['replace_exp'], exp['to_replace'])
+
+    for pattern in post_regex_expressions:
+        line = re.sub(
+            pattern['replace_exp'], 
+            pattern['to_replace'],
+            line
+        )
+
+    return line
+
+
 def get_pseudo_lines(filePath):
     with open(filePath, encoding='utf8') as rd:
         return rd.readlines()
@@ -83,6 +165,8 @@ def get_pseudo_lines(filePath):
 def create_python_file(filePath, lines):
     file_name = Path(filePath).stem + "_python.py"
     with open(file_name, 'w', encoding='utf8') as wf:
+        for line in header_lines:
+            print(line, file = wf)
         for line in lines:
             print(line, file = wf)
 
@@ -95,6 +179,10 @@ def pseudo2python(filePath):
         lines[i] = apply_basic_pattern(lines[i])
         lines[i] = apply_conditonal_pattern(lines[i])
         lines[i] = parse_linklist_line(lines[i])
+        lines[i] = look_for_stack_queues_expressions(lines[i])
+
+    for i in range(n):
+        lines[i] = apply_remaining_expressions(lines[i])
     
     create_python_file(filePath, lines)
 
